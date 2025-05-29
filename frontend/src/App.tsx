328094a09800e87fcee2c1a9c1906fd3c38aa267
Jonathan Hirsch <jonathan.hirsch2011@gmail.com>
2025-05-29 11:55:47 -0700

    Remove unused imports + simplify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,9 @@
-import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  type Transaction,
-  type TransactionFormData,
-} from "./types/transaction";
 import { AddTransactionForm } from "./components/AddTransactionForm/AddTransactionForm";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import "./App.css";
 
 function App() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  const addTransaction = (formData: TransactionFormData) => {
-    const newTransaction: Transaction = {
-      ...formData,
-      id: Date.now(),
-    };
-    setTransactions([newTransaction, ...transactions]);
-  };
-
   return (
     <div className="container mx-auto p-4 max-w-5xl">
       <header className="mb-8">
@@ -33,11 +18,11 @@ function App() {
         </TabsList>
 
         <TabsContent value="dashboard">
-          <Dashboard transactions={transactions} />
+          <Dashboard />
         </TabsContent>
 
         <TabsContent value="add">
-          <AddTransactionForm onAddTransaction={addTransaction} />
+          <AddTransactionForm />
         </TabsContent>
       </Tabs>
     </div>
